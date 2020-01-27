6a414708e21be94187fdfac0928feb7433c4ef8d
Felix Nilsen <felix1998nilsen@hotmail.com>
2020-01-27 15:37:27 +0100

    Added mockdata for products on frontpage. Removed Contact view

---
+++ ReleaseNotes/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReleaseNotes.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string productImage { get; set; }
+    }
+}
