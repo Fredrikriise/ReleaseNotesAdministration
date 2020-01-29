68ea2357779ac9c534c2e60f8d6f34db75f4b6de
Ocianus <fredrik1998@hotmail.com>
2020-01-29 19:57:20 +0100

    Fixed some styling and positioning of the realease notes mock data. Made a variable in the controller for mock data. Changed the format of datetime.

--- ReleaseNotes/Controllers/HomeController.cs
+++ ReleaseNotes/Controllers/HomeController.cs
@@ -38,11 +38,13 @@ namespace ReleaseNotes.Controllers
   
         public ActionResult ListReleaseNotes() 
         {
+            var bodytextData = "Lorem ipsum dolor sit amet, in nonummy lectus venenatis posuere risus ipsum, nulla vel lorem vitae bibendum sed, elit lacinia urna convallis eget placerat, duis wisi mauris nullam mauris, nulla vitae eu nunc nisl est.Odio justo dui ut nulla proin turpis, facere varius dolor eu ipsum congue orci, dolor lorem facilisis mauris euismod, viverra ipsum eros conubia tellus habitant. Mauris fusce egestas sodales rutrum, tellus odio tortor donec justo nec, aptent dictum dui elit mi dui, diam aliquam suscipit placerat, justo turpis integer sed.Leo ac eros ullamcorper eum sapien quam, ut quis felis, magna senectus fringilla eu ultricies vel, ac arcu sodales at urna sit mattis, nulla imperdiet quisque pede sit rutrum.Suscipit suspendisse. In hendrerit ipsum pellentesque aptent sollicitudin sapien, donec magna in cras in pulvinar quisque, eros adipiscing dui cursus hendrerit. Diam quam. Nunc elit elit semper in, nulla nam eros nonummy vestibulum suscipit, sed vitae. Vulputate ac sagittis amet nulla, ipsum aenean ante quis id duis, nisl nulla risus.";
+
             List<releaseNotes> releaseNotesList = new List<releaseNotes>
             {
                 new releaseNotes { 
                     title = "Release note 0.1 - Onboarding",
-                    bodytext = "Updated submit button to have round corners",
+                    bodytext = bodytextData,
                     id = 1,
                     productId = 1,
                     createdBy = "Fredrik Svevad Riise",
@@ -53,7 +55,7 @@ namespace ReleaseNotes.Controllers
                  
                 new releaseNotes {
                     title = "Release note 0.93 - Manager",
-                    bodytext = "Fixed log in errors",
+                    bodytext = bodytextData,
                     id = 2,
                     productId = 2,
                     createdBy = "Felix Thu Falkendal Nilsen",
--- ReleaseNotes/Models/ReleaseNotes.cs
+++ ReleaseNotes/Models/ReleaseNotes.cs
@@ -14,13 +14,19 @@ namespace ReleaseNotes.Models
         [Key]
         public int id { get; set; }
         public int productId { get; set; }
-        [Display(Name = "Creator")]
+        [Display(Name = "Posted by")]
         public string createdBy { get; set; }
         [Display(Name = "Date published")]
+        //Formats the datetime to only show day, month and year
+        //ApplyFormatInEditMode is used when entering a date, or picking a date, so unless we will use either of those features then we can remove "ApplyFormatInEditMode"
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime? createdDate { get; set; }
         [Display(Name = "Last updated by")]
         public string lastUpdatedBy { get; set; }
         [Display(Name = "Last updated")]
+        //Formats the datetime to only show day, month and year
+        //ApplyFormatInEditMode is used when entering a date, or picking a date, so unless we will use either of those features then we can remove "ApplyFormatInEditMode"
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime? lastUpdatedDate { get; set; }
 
     }
