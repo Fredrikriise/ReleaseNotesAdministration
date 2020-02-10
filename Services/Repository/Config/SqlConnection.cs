687b4915e674b19f1548b65becb6fef039197629
Felix Nilsen <felix1998nilsen@hotmail.com>
2020-02-10 17:24:09 +0100

    Added classes and methods for database connection (not finished)

---
+++ Services/Repository/Config/SqlConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Repository.Config
+{
+    class SqlConnection
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Services/Repository/Models/DatabaseModels/ReleaseNote.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Repository.Models.DatabaseModels
+{
+    class ReleaseNote
+    {
+    }
+}
