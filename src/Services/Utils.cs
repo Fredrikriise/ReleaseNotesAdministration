e1f023effe4b830257feef7786d9ba4ba555be1a
Ocianus <fredrik1998@hotmail.com>
2020-02-25 16:23:01 +0100

    Added mock classes for learning testing

---
+++ src/Services/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class Utils
+    {
+        string Title;
+        string BodyText;
+        int Id;
+        int ProductId;
+        string CreatedBy;
+        DateTime CreatedDate;
+        string LastUpdatedBy;
+        DateTime LastUpdateDate;
+
+        public void setValue()
+        {
+            Title = "Release notes 0.93 - Talent Onboarding";
+            BodyText = "Lorem ipsum oasfjhifhiwhgywhghwi";
+            Id = 1;
+            ProductId = 1;
+            CreatedBy = "Fredrik Riise";
+            CreatedDate = DateTime.Now;
+
+        }
+
+        public string DoStuff(string stuff)
+        {
+            return "Fredrik";
+        }
+    }
+}
---
+++ test/Services/UtilsTests.cs
@@ -0,0 +1,19 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace test.Services
+{
+    public class UtilsTests
+    {
+        [Fact]
+        public void TestStuff()
+        {
+            var utils = new Utils();
+            var name = "Fredrik";
+            //Assert.Equal(name, utils.DoStuff("Fredrik"));
+        }
+    }
+}
