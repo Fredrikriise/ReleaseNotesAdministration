06478e55fcb8e788221c91b1c134eb9064db750d
Ocianus <fredrik1998@hotmail.com>
2020-02-26 17:46:11 +0100

    Added required attribute

--- src/ReleaseNotesAdministration/ViewModels/ReleaseNoteAdminViewModel.cs
+++ src/ReleaseNotesAdministration/ViewModels/ReleaseNoteAdminViewModel.cs
@@ -5,17 +5,22 @@ namespace ReleaseNotesAdministration.ViewModels
 {
     public class ReleaseNoteAdminViewModel
     {
+        [Required(ErrorMessage ="Title is required to create a new release note!")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Body text is required to create a new release note!")]
         public string BodyText { get; set; }
 
         [Key]
         public int? Id { get; set; }
 
+        [Required(ErrorMessage = "Product id is required to create a new release note!")]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = "Author is required to create a new release note!")]
         public string CreatedBy { get; set; }
 
+        [Required(ErrorMessage = "Created date is required to create a new release note!")]
         public DateTime? CreatedDate { get; set; }
 
         public string LastUpdatedBy { get; set; }
