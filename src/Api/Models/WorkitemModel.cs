d6fae311a72b5fb937a4426271bfa33f872141ed
Ocianus <fredrik1998@hotmail.com>
2020-02-26 14:49:49 +0100

    - Added controller for work items - Fixed small typo's in releasenotecontroller - Added class WorkItemList and WorkItemModel -Added mapping between WorkItem and WorkItemDto

---
+++ src/Api/Models/WorkitemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class WorkItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AssignedTo { get; set; }
+        public string State { get; set; }
+    }
+}
