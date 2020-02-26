f2601ddf09a4075a4114eb8f7f3b79c0c25c13ac
Ocianus <fredrik1998@hotmail.com>
2020-02-26 14:00:27 +0100

    - Added class for models and database models for work items - Added repository with queries for work items - Added interface for repository - Fixed variabe names in releasenotesrepository

---
+++ src/Services/Repository/Models/DataTransferObjects/WorkItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Repository.Models.DataTransferObjects
+{
+    public class WorkItemDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AssignedTo { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ src/Services/Repository/Models/DatabaseModels/WorkItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Repository.Models.DatabaseModels
+{
+    public class WorkItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AssignedTo { get; set; }
+        public string State { get; set; }
+    }
+}
