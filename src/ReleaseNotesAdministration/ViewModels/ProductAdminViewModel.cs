5c41935c3b7d5e5b4fd503a87ca6c7e2d7edc23e
Ocianus <fredrik1998@hotmail.com>
2020-02-28 09:49:48 +0100

    Added some frontend validation, currently using [Required] tag.

--- src/ReleaseNotesAdministration/ViewModels/ProductAdminViewModel.cs
+++ src/ReleaseNotesAdministration/ViewModels/ProductAdminViewModel.cs
@@ -11,10 +11,13 @@ namespace ReleaseNotesAdministration.ViewModels
         [Key]
         public int? ProductId { get; set; }
 
+        [Required(ErrorMessage = "Product name is required to create a new product!")]
         public string ProductName { get; set; }
 
+        [Required(ErrorMessage = "Image url is required to create a new product!")]
         public string ProductImage { get; set; }
 
+        [Required(ErrorMessage = "Product description is required to create a new product!")]
         public string ProductDescription { get; set; }
     }
 }
