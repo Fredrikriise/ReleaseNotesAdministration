47b152bf7f94b31404d4cc015dfc431d0cee466c
Felix Nilsen <felix1998nilsen@hotmail.com>
2020-02-28 13:27:02 +0100

    * Changed styling and search bar in ListReleaseNotes

--- src/ReleaseNotes/QuestionsAndToDo.cs
+++ src/ReleaseNotes/QuestionsAndToDo.cs
@@ -4,17 +4,25 @@
     {
         /* 
         // QUESTIONS
-        - Popups for confirmation av CRUD operasjoner: hva skal brukes for dette?
-        - Migrering for database?
-        - Bør alle cardsa for work items være like store? 
-        - Er det en annen måte enn å bruke Razor tags i delete view, eller har det ikke noe særlig å si? Vi bruker tag helpers i de andre viewene. 
+        - Trenger vi feilhåndtering i API Controllere?
+        - Popups for confirmation av CRUD operasjoner: Skal det brukes bootstrap for dette? Toaster notifications
+        - Migrering for database? Spørre henning
+        - Bør alle cardsa for work items være like store? Ja
         
 
         // ToDo's
         - See work items
 
         I dag:
-        1 Ta en clean up!
+        1 Fikse begge views for delete (Fjerne @Html tags)
+        2 Ta en clean up!
+
+        // Spørsmål til møtet fredag 28/02 kl. 12
+        - Skal vi bruke model state?
+        - Eget view etter at man har valgt work item, hvor data blit listet i input fields + text editor?
+        - Created by/updated by så henter vi vell bare brukernavnet? Eller kanskje ikke brukerne dems i hrid bruker fulle navn, da må vi kanskje bruke input fields?
+        - Når man skal hente data direkte fra API, f.eks. API-localhost/ReleaseNotes/Id, så får man error dersom id'en ikke finnes i databasen, er dette noe som må fikses? 
+        - Skal vi lage et kontrollpanel?
 
 
         */
