cb6679299125dff76c60aeb3adf0f13b69cb5f24
Ocianus <fredrik1998@hotmail.com>
2020-03-12 13:23:49 +0100

    Added unit test for updateproduct and getallproducts (api)

---
+++ test/ReleaseNotes/Controllers/ProductControllerShould.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ReleaseNotes.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace test.ReleaseNotes.Controllers
+{
+    public class ProductControllerShould
+    {
+
+    }
+}
