366c311990e056793a253e22dbb0c90214c99bbb
Felix Stasjonaer <felix1998nilsen@hotmail.com>
2020-04-28 14:35:15 +0200

    * commented out repo test for product

--- test/ServicesTests/Repository/ProductRepositoryTest.cs
+++ test/ServicesTests/Repository/ProductRepositoryTest.cs
@@ -10,25 +10,21 @@ namespace test.ServicesTests.Repository
 {
     public class ProductRepositoryTest
     {
-        private readonly ProductsRepository _productsRepo;
-#pragma warning disable CS0649 // Field 'ProductRepositoryTest._mapper' is never assigned to, and will always have its default value null
-        private readonly IMapper _mapper;
-#pragma warning restore CS0649 // Field 'ProductRepositoryTest._mapper' is never assigned to, and will always have its default value null
+//        private readonly ProductsRepository _productsRepo;
+//        private readonly IMapper _mapper;
 
-        public ProductRepositoryTest(DatabaseFixture database)
-        {
-            _productsRepo = new ProductsRepository(Options.Create(new SqlDbConnection
-            {
-                ConnectionString = database.ConnectionString
-            }), _mapper);
-        }
+//        public ProductRepositoryTest(DatabaseFixture database)
+//        {
+//            _productsRepo = new ProductsRepository(Options.Create(new SqlDbConnection
+//            {
+//                ConnectionString = database.ConnectionString
+//            }), _mapper);
+//        }
 
-        [Fact]
-#pragma warning disable CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
-        public async Task GetAllProducts_Should_ReturnMappedProduct()
-#pragma warning restore CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
-        {
+//        [Fact]
+//        public async Task GetAllProducts_Should_ReturnMappedProduct()
+//        {
 
-        }
+//        }
     }
 }
