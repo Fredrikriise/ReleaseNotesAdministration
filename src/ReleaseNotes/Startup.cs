e78267700e51509f7dd7cdaa79438b582026e6bf
Ocianus <fredrik1998@hotmail.com>
2020-05-14 23:12:05 +0200

    Cleanup

--- src/ReleaseNotes/Startup.cs
+++ src/ReleaseNotes/Startup.cs
@@ -1,16 +1,11 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 
-namespace ReleaseNotes 
+namespace ReleaseNotes
 {
     public class Startup
     {
